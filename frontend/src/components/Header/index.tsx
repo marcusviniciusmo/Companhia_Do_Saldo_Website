daab1c7fd49057aeda981a686ad50e077a0f907d
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-11-22 22:17:59 +0000

    Header component creation

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+export function Header() {
+  return (
+    <>
+      <h1>HEADER Component</h1>
+      <h1>A melhor malha está aqui</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h1>MENU</h1>
+      <h1>Menu Principal</h1>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Home</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Produtos</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Fornecedores</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Pedidos</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Contato</h4>
+    </>
+  );
+};
