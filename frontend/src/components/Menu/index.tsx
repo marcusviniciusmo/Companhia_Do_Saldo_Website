c4d7143ad4119ba44708bfd07d4d687859f2a816
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-11-22 22:30:33 +0000

    Navigation through the views by the Menu component

--- frontend/src/components/Menu/index.tsx
+++ frontend/src/components/Menu/index.tsx
@@ -1,18 +1,31 @@
+import { Link } from 'react-router-dom';
+
 export function Menu() {
   return (
     <>
       <h1>MENU Component</h1>
       <h1>Menu Principal</h1>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4>Home</h4>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4>Produtos</h4>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4>Fornecedores</h4>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4>Pedidos</h4>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4>Contato</h4>
+
+      <Link to='/'>
+        <h4>Home</h4>
+      </Link>
+
+      <Link to='/products'>
+        <h4>Produtos</h4>
+      </Link>
+
+      <Link to='/suppliers'>
+        <h4>Fornecedores</h4>
+      </Link>
+
+      <Link to='/orders'>
+        <h4>Pedidos</h4>
+      </Link>
+
+      <Link to='/contact'>
+        <h4>Contato</h4>
+      </Link>
+
     </>
   );
 };
