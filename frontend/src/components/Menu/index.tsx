ea85ece9e63f8a8d6ab7f6e919bfa82e63abdc16
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-11-22 22:26:24 +0000

    Menu component creation

---
+++ frontend/src/components/Menu/index.tsx
@@ -0,0 +1,18 @@
+export function Menu() {
+  return (
+    <>
+      <h1>MENU Component</h1>
+      <h1>Menu Principal</h1>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Home</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Produtos</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Fornecedores</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Pedidos</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Contato</h4>
+    </>
+  );
+};
