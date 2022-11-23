7c9dc1c0162a4de3cc613cd17fcf4caae1defc95
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-11-23 11:16:53 +0000

    Localization component styling

--- frontend/src/components/Localization/index.tsx
+++ frontend/src/components/Localization/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Map } from "../Map";
 import { LocalizationProps } from "../../types/Localization";
 import { MockedData } from "../../mocks/Localization";
+import { Container, Paragraph, Title } from './styles';
 
 export function Localization() {
   const [mockedData, setMockedData] = useState<LocalizationProps>();
@@ -11,18 +12,15 @@ export function Localization() {
   }, []);
 
   return (
-    <>
-      <h1>LOCALIZATION Component</h1>
-
-      <h2>{mockedData?.title}</h2>
-      <p>
-        {mockedData?.addressLine1}
-        {mockedData?.addressLine2}
-        {mockedData?.addressLine3}
-        {mockedData?.reference}
-      </p>
+    <Container>
+      <Title className='articleTitle'>{mockedData?.title}</Title>
+      <Paragraph>{mockedData?.addressLine1}</Paragraph>
+      <Paragraph>{mockedData?.addressLine2}</Paragraph>
+      <Paragraph>{mockedData?.addressLine3}</Paragraph>
+      <Paragraph><em>{mockedData?.reference}</em></Paragraph>
+      
 
       <Map />
-    </>
+    </Container>
   );
 };
---
+++ frontend/src/components/Localization/styles.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.article``;
+
+export const Title = styled.h2``;
+
+export const Paragraph = styled.p`
+  font-size: 1.5rem;
+  text-align: left;
+  margin-block: 1rem;
+  padding-left: 6rem;
+`;
