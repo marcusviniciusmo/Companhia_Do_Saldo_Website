0bff4332d82ca798694cc4178c546a02fcc9027a
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-11-23 22:19:44 +0000

    Finishing the Produtos view styling

--- frontend/src/views/Products/index.tsx
+++ frontend/src/views/Products/index.tsx
@@ -2,16 +2,13 @@ import { Header } from "../../components/Header";
 import { Breadcrumb } from "../../components/Breadcrumb";
 import { Gallery } from "../../components/Gallery";
 import { Footer } from "../../components/Footer";
+import { Interface } from "../../styles/Interface";
 
 export function Products() {
   return (
-    <>
-      <h1>VIEW PRODUTOS</h1>
-      <h1>Companhia do Saldo - Página Produtos</h1>
-
+    <Interface>
       <Header />
 
-      <h1>BODY</h1>
       <Breadcrumb
         path='Produtos &gt; Galeria'
         title='Produtos'
@@ -20,6 +17,6 @@ export function Products() {
       <Gallery />
 
       <Footer />
-    </>
+    </Interface>
   );
 };
