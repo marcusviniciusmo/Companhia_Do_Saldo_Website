2251e587d2bea96773de68daf707630f5a898b5b
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-11-23 07:23:46 +0000

    Creating a function that returns the current year to Footer component

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,9 +1,11 @@
+import { GetCurrentYear } from "../../utils/Functions";
+
 export function Footer() {
   return (
     <>
       <h1>FOOTER Component</h1>
       <p>
-        Copyright &copy; 2016 Companhia do Saldo. Todos os direitos reservados.
+        Copyright &copy; {GetCurrentYear()} Companhia do Saldo. Todos os direitos reservados.
         Facebook
         Instagram
         Home
---
+++ frontend/src/utils/Functions/index.tsx
@@ -0,0 +1,5 @@
+export function GetCurrentYear() {
+  const now = new Date();
+
+  return now.getFullYear();
+};
