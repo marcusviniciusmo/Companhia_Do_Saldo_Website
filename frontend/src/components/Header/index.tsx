c138810bf377a335cb4753ef8cc78593bc4832f2
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-11-23 06:52:38 +0000

    Image logo placed in the Header component

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { Menu } from "../Menu";
+import Logo from '../../assets/header/logo.png';
 
 export function Header() {
   return (
@@ -6,6 +7,7 @@ export function Header() {
       <h1>HEADER Component</h1>
       <h1>A melhor malha está aqui</h1>
       <h3>AQUI VAI UMA IMAGEM</h3>
+      <img src={Logo} alt="Logo Companhia do Saldo" />
       
       <Menu />
     </>
