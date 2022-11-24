3c221a9f650d64e31b794843725f4d574b420b61
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-11-24 17:13:55 +0000

    Create data typing for the Input component

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -1,5 +1,9 @@
-export function Input() {
+import { InputProps } from "../../types/Input";
+
+export function Input(props: InputProps) {
   return (
-    <h1>INPUT Component</h1>
+    <>
+      <h1>INPUT Component</h1>
+    </>
   );
 };
