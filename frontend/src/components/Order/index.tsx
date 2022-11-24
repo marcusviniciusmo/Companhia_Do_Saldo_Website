bd5c03f7a45e9aa06b998def8419d5e1905237eb
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-11-24 10:19:19 +0000

    Order component creation

---
+++ frontend/src/components/Order/index.tsx
@@ -0,0 +1,52 @@
+export function Order() {
+  return (
+    <>
+      <h1>ORDER Component</h1>
+
+      <p>
+        Gostaria de facilitar suas compras? Se cadastre, e com isso, você poderá
+        fazer seus pedidos, sem sair de casa.
+      </p>
+
+      <h3>FORMULÁRIO</h3>
+      <h3>Identificação do Cliente</h3>
+      <h4>Nome:</h4>
+      <h4>AQUI VAI UM CAMPO</h4>
+      <h4>E-mail:</h4>
+      <h4>AQUI VAI UM CAMPO</h4>
+      <h4>Sexo:</h4>
+      <h4>AQUI VAI UM CAMPO</h4>
+      <h4>Masculino</h4>
+      <h4>AQUI VAI UM CAMPO</h4>
+      <h4>Feminino</h4>
+      <h4>Data de Nascimento:</h4>
+      <h4>AQUI VAI UM CAMPO</h4>
+
+      <h3>Endereço do Cliente</h3>
+      <h4>Logradouro:</h4>
+      <h4>AQUI VAI UM CAMPO</h4>
+      <h4>Número:</h4>
+      <h4>AQUI VAI UM CAMPO</h4>
+      <h4>Estado:</h4>
+      <h4>AQUI VAI UM CAMPO</h4>
+      <h4>Cidade:</h4>
+      <h4>AQUI VAI UM CAMPO</h4>
+
+      <h3>Mensagem do Cliente</h3>
+      <h4>Grau de Urgência:</h4>
+      <h4>AQUI VAI UM CAMPO</h4>
+      <h4>Mensagem:</h4>
+      <h4>AQUI VAI UM CAMPO</h4>
+
+      <h3>Quero comprar</h3>
+      <h4>Produto:</h4>
+      <h4>AQUI VAI UM CAMPO</h4>
+      <h4>Cor:</h4>
+      <h4>AQUI VAI UM CAMPO</h4>
+      <h4>Quantidade:</h4>
+      <h4>AQUI VAI UM CAMPO</h4>
+
+      <h3>AQUI VAI UM BOTÃO</h3>
+    </>
+  );
+};
--- frontend/src/views/Orders/index.tsx
+++ frontend/src/views/Orders/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Breadcrumb } from "../../components/Breadcrumb";
+import { Order } from "../../components/Order";
 import { Footer } from "../../components/Footer";
 
 export function Orders() {
@@ -16,50 +17,7 @@ export function Orders() {
         title='Formulário de Cadastro'
       />
 
-      <p>
-        Gostaria de facilitar suas compras? Se cadastre, e com isso, você poderá
-        fazer seus pedidos, sem sair de casa.
-      </p>
-
-      <h3>FORMULÁRIO</h3>
-      <h3>Identificação do Cliente</h3>
-      <h4>Nome:</h4>
-      <h4>AQUI VAI UM CAMPO</h4>
-      <h4>E-mail:</h4>
-      <h4>AQUI VAI UM CAMPO</h4>
-      <h4>Sexo:</h4>
-      <h4>AQUI VAI UM CAMPO</h4>
-      <h4>Masculino</h4>
-      <h4>AQUI VAI UM CAMPO</h4>
-      <h4>Feminino</h4>
-      <h4>Data de Nascimento:</h4>
-      <h4>AQUI VAI UM CAMPO</h4>
-
-      <h3>Endereço do Cliente</h3>
-      <h4>Logradouro:</h4>
-      <h4>AQUI VAI UM CAMPO</h4>
-      <h4>Número:</h4>
-      <h4>AQUI VAI UM CAMPO</h4>
-      <h4>Estado:</h4>
-      <h4>AQUI VAI UM CAMPO</h4>
-      <h4>Cidade:</h4>
-      <h4>AQUI VAI UM CAMPO</h4>
-
-      <h3>Mensagem do Cliente</h3>
-      <h4>Grau de Urgência:</h4>
-      <h4>AQUI VAI UM CAMPO</h4>
-      <h4>Mensagem:</h4>
-      <h4>AQUI VAI UM CAMPO</h4>
-
-      <h3>Quero comprar</h3>
-      <h4>Produto:</h4>
-      <h4>AQUI VAI UM CAMPO</h4>
-      <h4>Cor:</h4>
-      <h4>AQUI VAI UM CAMPO</h4>
-      <h4>Quantidade:</h4>
-      <h4>AQUI VAI UM CAMPO</h4>
-
-      <h3>AQUI VAI UM BOTÃO</h3>
+      <Order />
 
       <Footer />
     </>
