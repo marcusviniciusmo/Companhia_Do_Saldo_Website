8fdd05e31ab01591214f319e24f768c77f0b55b0
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-11-24 11:34:20 +0000

    Creating the routes for the Iframe views in the Orders view

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -4,6 +4,10 @@ import { Products } from "../views/Products";
 import { Suppliers } from "../views/Suppliers";
 import { Orders } from "../views/Orders";
 import { Contact } from "../views/Contact";
+import { Identification } from '../components/Identification';
+import { Address } from '../components/Address';
+import { Message } from '../components/Message';
+import { Product } from '../components/Product';
 
 export function Router() {
   return (
@@ -13,6 +17,10 @@ export function Router() {
         <Route path='/products' element={<Products />} />
         <Route path='/suppliers' element={<Suppliers />} />
         <Route path='/orders' element={<Orders />} />
+          <Route path='/orders/identification' element={<Identification />} />
+          <Route path='/orders/address' element={<Address />} />
+          <Route path='/orders/message' element={<Message />} />
+          <Route path='/orders/product' element={<Product />} />
         <Route path='/contact' element={<Contact />} />
       </Routes>
     </BrowserRouter>
