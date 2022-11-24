50250ed3a2b1eb64b157883350f63cadb10bdc1f
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-11-24 17:17:54 +0000

    Create data typing fot the Textarea component

--- frontend/src/components/Textarea/index.tsx
+++ frontend/src/components/Textarea/index.tsx
@@ -1,4 +1,6 @@
-export function Textarea() {
+import { TextareaProps } from "../../types/Textarea";
+
+export function Textarea(props: TextareaProps) {
   return (
     <h1>TEXTAREA Component</h1>
   );
---
+++ frontend/src/types/Textarea/index.tsx
@@ -0,0 +1,3 @@
+import { TextareaHTMLAttributes } from "react";
+
+export interface TextareaProps extends TextareaHTMLAttributes<HTMLElement> {};
