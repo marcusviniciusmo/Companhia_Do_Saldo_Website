0a7b0fc527c3b3ffa9f70fe87d3122cbe0021544
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-11-24 06:48:49 +0000

    Supplier component creation

---
+++ frontend/src/components/Supplier/index.tsx
@@ -0,0 +1,141 @@
+export function Supplier() {
+  return (
+    <>
+      <h1>SUPPLIER Component</h1>
+
+      <p>
+        Nós crescemos junto com nossos clientes. Conheça nossos parceiros e veja
+        a equipe de peso por trás do seu sucesso.
+      </p>
+
+      <h1>Adar</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <p>
+        "A moda é a maior paixão da Adar Tecidos. Imersos neste universo, estamos
+        na busca constante pelo inusitado, criando produtos que transpiram
+        inovação, para um público cheio de desejos. Cliente em primeiro lugar. O
+        foco do nosso trabalho é o sucesso dos nossos clientes, e, para isso,
+        procuramos oferecer produtos e serviços que superem suas expectativas."
+      </p>
+
+      <h1>Indaial</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <p>
+        "Há mais de 20 anos oferecendo soluçōes em tecidos de malhas para os
+        clientes, com consciência de preservação ambiental e ambiente de trabalho
+        digno e honrado, a Malharia Indaial tem como objetivo principal atender
+        às necessidades do mercado com agilidade e competitividade.
+        A equipe profissional da malharia é composta por 120 colaboradores
+        capacitados e dedicados a atender o país de Norte a Sul, comprometidos
+        com a empresa e com os clientes."
+      </p>
+
+      <h1>Jangadeiro</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <p>
+        "A Jangadeiro Têxtil ao longo dos anos se especializou na arte de
+        transformar fios em malhas, desenvolvendo produtos e fabricando artigos
+        com matérias primas diferenciadas. Aposta em pesquisa de tendências de
+        moda, sempre buscando os comportamentos nacionais e internacionais. Isso
+        à motiva para a realização de grandes e constantes investimentos em
+        inovação e tecnologia, o que vem consolidá-la como uma marca de
+        referência nacional."
+      </p>
+
+      <h1>Latina</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <p>
+        "Tradição e credibilidade marcam a trajetória da Latina em seus mais de
+        20 anos de atuação no mercado têxtil. A partir de insumos altamente
+        selecionados, produzimos tecidos para Moda e Moda Íntima, desenvolvendo
+        produtos que se destacam em qualidade, conforto e estilo...Um cliente
+        Latina tem sempre à sua disposição as melhores tendências de moda e
+        inovação; agregados à um atendimento personalizado de pessoas que têm
+        paixão pelo que fazem!"
+      </p>
+
+      <h1>Luli</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <p>
+        "Desde que começou, em janeiro de 1985 como uma modesta confecção, até
+        os dias atuais a Luli vem surpreendendo e conquistando clientes com suas
+        malhas, confecçōes e serviços com criatividade e qualidade superior. A
+        Luli é hoje uma empresa verticalizada, morderna, com equipamentos de
+        última geração e cerca de 1.000 colaboradores."
+      </p>
+
+      <h1>Quatro K</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <p>
+        "No mercado têxtil hà mais de 30 anos, a história da Quatro K começou em
+        1987 na cidade de Santos, onde iniciou suas atividades como confecção, e
+        posteriormente partiu para a venda de malhas e de toda a matéria prima
+        do processo produtivo.
+        A Quatro K Têxtil destaca-se como uma grande empresa empreendedora e
+        única no Brasil através de um formato de gestão inédito, onde
+        gerenciamos a produção de malhas de maneira vertical e terceirizada,
+        atuando em todas as etapas do processo produtivo."
+      </p>
+
+      <h1>Rei do Cotton</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <p>
+        "Confiança e qualidade, nós crescemos com você. A loja de fábrica em
+        Fortaleza que produz tecidos em alta qualidade e preço baixo."
+      </p>
+
+      <h1>Stamp Lite</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <p>
+        "Nascida em 1995 num canto de um galpão de um amigo, a Stamp Lite teve
+        um grande aumento nos negócios, que levou-a em apenas quatro meses, à um
+        galpão de 400m². Isto se deve ao alto nível de automação com
+        gerenciamento eletrônico constantemente avaliado por nosso laboratório
+        de análises voltado a identificar os menores desvios dos padrōes
+        estabelecidos, oferecendo relatórios a curtíssimos prazos, minimizando
+        distorçōes.
+        Possuindo mais de uma máquina para cada fase da produção, pode manter
+        seu estoque de pronta entrega em níveis adequados ou refazê-los em um
+        curto prazo."
+      </p>
+
+      <h1>Stik</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <p>
+        "Beleza que encanta consumidores. Pensando em suas necessidades e em sua
+        satisfação, buscamos a excelência em cada produto que fabricamos,
+        aliando talento e competência ao enorme potencial criativo de nossos
+        colaboradores. O novo e o antigo convivem harmonicamente, numa releitura
+        constante do clássico."
+      </p>
+
+      <h1>Trichê</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <p>
+        "Em todos os produtos Trichê, sempre houve preocupação quanto à
+        qualidade e, extrema dedicação quanto aos suprimentos das necessidades,
+        satisfação e respeito ao cliente, o que com certeza em muito tem
+        contribuído para o constante desenvolvimento da Trichê."
+      </p>
+
+      <h1>Wilson</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <p>
+        "Em 10 de janeiro de 1974, dá início a empresa têxtil denominada Malhas
+        Wilson LTDA. Com nova gestão e acompanhando as necessidades do mercado,
+        a empresa avançou em sua estrutura física, tecnológica, maquinários e
+        treinamentos com o objetivo de atender a crescente demanda por seus
+        produtos. Conta hoje com mais de 250 colaboradores que garantem o
+        sucesso da empresa e prezam a qualidade no atendimento de seus
+        clientes."
+      </p>
+
+      <h1>Zanotti</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <p>
+        "Zanotti, referência na fabricação de fitas elásticas em toda a América.
+        11 milhōes de metros produzidos por dia."
+      </p>
+    </>
+  );
+};
