21b0ebf713f27cff7f7019d129b0e0384ea476d8
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-11-24 17:15:49 +0000

    Create data typing for the Select component

--- frontend/src/components/Select/index.tsx
+++ frontend/src/components/Select/index.tsx
@@ -1,4 +1,6 @@
-export function Select() {
+import { SelectProps } from "../../types/Select";
+
+export function Select(props: SelectProps) {
   return (
     <h1>SELECT Component</h1>
   );
---
+++ frontend/src/types/Select/index.tsx
@@ -0,0 +1,3 @@
+import { SelectHTMLAttributes } from "react";
+
+export interface SelectProps extends SelectHTMLAttributes<HTMLElement> {};
