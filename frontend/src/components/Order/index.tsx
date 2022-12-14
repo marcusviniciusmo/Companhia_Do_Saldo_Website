c928a88353a55a3ae2093cf523fe8bda30aa2b29
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-12-14 14:39:55 +0000

    Create data typing fot the Order component

--- frontend/src/components/Order/index.tsx
+++ frontend/src/components/Order/index.tsx
@@ -1,14 +1,20 @@
 import { Form } from "../Form";
+import { OrderProps } from "../../types/Order";
+import { MockedData } from "../../mocks/Order";
+import { useEffect, useState } from "react";
 
 export function Order() {
+  const [mockedData, setMockedData] = useState<OrderProps>();
+
+  useEffect(() => {
+    setMockedData(MockedData)
+  }, []);
+
   return (
     <>
       <h1>ORDER Component</h1>
 
-      <p>
-        Gostaria de facilitar suas compras? Se cadastre, e com isso, você poderá
-        fazer seus pedidos, sem sair de casa.
-      </p>
+      <p>{mockedData?.text}</p>
 
       <Form />
     </>
---
+++ frontend/src/mocks/Order/index.tsx
@@ -0,0 +1,6 @@
+import { OrderProps } from "../../types/Order";
+
+export const MockedData: OrderProps = {
+  text: `Gostaria de facilitar suas compras? Se cadastre, e com isso, você
+    poderá fazer seus pedidos, sem sair de casa.`
+};
