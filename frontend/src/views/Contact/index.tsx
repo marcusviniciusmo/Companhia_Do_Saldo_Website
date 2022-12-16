d19bd872de90b7232bc96a9591b850bd58896638
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-12-16 09:06:58 +0000

    Contact view styling

--- frontend/src/views/Contact/index.tsx
+++ frontend/src/views/Contact/index.tsx
@@ -2,16 +2,13 @@ import { Header } from "../../components/Header";
 import { Breadcrumb } from "../../components/Breadcrumb";
 import { Footer } from "../../components/Footer";
 import { ContactForm } from "../../components/ContactForm";
+import { Interface } from "../../styles/Interface";
 
 export function Contact() {
   return (
-    <>
-      <h1>VIEW CONTATO</h1>
-      <h1>Companhia do Saldo - Página Contato</h1>
-
+    <Interface>
       <Header />
 
-      <h1>BODY</h1>
       <Breadcrumb
         path='Contato &gt; Fale Conosco'
         title='Sugestōes e Reclamaçōes'
@@ -20,6 +17,6 @@ export function Contact() {
       <ContactForm />
 
       <Footer />
-    </>
+    </Interface>
   );
 };
